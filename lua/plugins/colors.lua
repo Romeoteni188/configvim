012e7f26b700b9df474cf0a7147af9f779e34bfa
Romeoteni188 <Romeoteni093@gmail.com>
2025-02-15 18:58:42 -0600

    first commit

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,9 @@
+return {
+  { "maxmx03/solarized.nvim" },
+  { "ronisbr/nano-theme.nvim" },
+  { "ku1ik/vim-monokai" },
+  { "timmyha/henna.nvim" },
+  { "nyoom-engineering/oxocarbon.nvim" },
+  { "https://github.com/folke/tokyonight.nvim" },
+  { "https://github.com/navarasu/onedark.nvim" },
+}
